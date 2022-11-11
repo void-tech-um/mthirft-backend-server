3fa4051d3707914ab8b05dd610370552c4aa9ea7
jennweir <91640709+jennweir@users.noreply.github.com>
2022-11-11 13:45:21 -0500

    Update src/models/user.repository.ts
    Co-authored-by: Santiago Medina <santiagm08@gmail.com>

--- src/models/user.repository.ts
+++ src/models/user.repository.ts
@@ -24,7 +24,7 @@ export const createUser = async (user: User): Promise<User | boolean> => {
 /**
  * @desc Check if user exists and return user
  * @param {User} user object
- * @returns {Promise<User | null>} Username or false if user does not exist or password is incorrect
+ * @returns {Promise<User | null>} Username or null if user does not exist or password is incorrect
  */
 export const loginUser = async (user: User): Promise<User | null> => {
   // Check if user exists
